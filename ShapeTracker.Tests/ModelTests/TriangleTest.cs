3ff5de2ad65215c64672a6d84d80816f6456fa00
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-01-15 15:46:06 -0800

    passing five tests

--- ShapeTracker.Tests/ModelTests/TriangleTest.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTest.cs
@@ -8,17 +8,43 @@ namespace ShapeTracker.Tests
         [TestMethod]
         public void TriangleConstructor_CreateInstanceOfTriangle_Triangle()
         {
-            Triangle newTriangle = new Triangle(3);
+            Triangle newTriangle = new Triangle(3, 6);
             Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
         }
 
         [TestMethod]
         public void GetSide1_ReturnsSide1_Int()
         {
+            // Arrange
             int length1 = 3;
-            Triangle newTriangle = new Triangle(length1);
-            int result = newTriangle.Side1;
-            Assert.AreEqual(length1, result);
+            Triangle newTriangle = new Triangle(length1, 8);
+            Assert.AreEqual(length1, newTriangle.Side1);
+        }
+
+        [TestMethod]
+        public void SetSide1_SetsValueOfSide1_Void()
+        {
+            Triangle newTriangle = new Triangle(3, 6);
+            int newLength = 34;
+            newTriangle.Side1 = newLength;
+            Assert.AreEqual(newLength, newTriangle.Side1);
+        }
+
+        [TestMethod]
+        public void GetSide2_ReturnsSide2_Int()
+        {
+            int length2 = 3;
+            Triangle newTriangle = new Triangle(2, length2);
+            Assert.AreEqual(length2, newTriangle.Side2);
+        }
+
+        [TestMethod]
+        public void SetSide2_SetsValueOfSide2_Void()
+        {
+            Triangle newTriangle = new Triangle(4, 3);
+            int newSide2 = 5;
+            newTriangle.Side2 = newSide2;
+            Assert.AreEqual(newSide2, newTriangle.Side2);
         }
     }
 }
