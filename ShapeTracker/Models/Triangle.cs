be5ccd9a9bcceab0d3287149aa0f9ebae60847d4
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-01-15 14:36:16 -0800

    update get and set mechanisms

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -1,59 +1,67 @@
 namespace ShapeTracker.Models
 {
-  public class Triangle
-{
-  private int _side1;
-  private int _side2;
-  private int _side3;
+    public class Triangle
+    {
+        private int _side1;
+        public int Side1
+        {
+            get { return _side1; }
+            set { _side1 = value; }
+        }
+        private int _side2;
+        public int Side2
+        {
+            get { return _side2; }
+            set { _side2 = value; }
+        }
+        private int _side3;
+        public int Side3
+        {
+            get { return _side3; }
+            set { _side3 = value; }
+        }
+        private static List<Triangle> _instances = new List<Triangle> { };
 
-  public Triangle(int length1, int length2, int length3)
-  {
-    _side1 = length1;
-    _side2 = length2;
-    _side3 = length3;
-  }
-  public int GetSide1()
-  {
-    return _side1;
-  }
-  public int GetSide2()
-  {
-    return _side2;
-  }
-  public int GetSide3()
-  {
-    return _side3;
-  }
-  }
-  public void SetSide1(int value)
-  {
-    _side1 = value;
-  }
-  public void SetSide2(int value)
-  {
-    _side2 = value;
+        public Triangle(int length1, int length2, int length3)
+        {
+            _side1 = length1;
+            _side2 = length2;
+            _side3 = length3;
+            _instances.Add(this);
+        }
+
+        public static List<Triangle> GetAll()
+        {
+            return _instances;
+        }
+
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
+
+        public string CheckType()
+        {
+            if (
+                (_side1 > (_side2 + _side3))
+                || (_side2 > (_side1 + _side3))
+                || (_side3 > (_side1 + _side2))
+            )
+            {
+                return "not a triangle";
+            }
+            else if ((_side1 != _side2) && (_side1 != _side3) && (_side2 != _side3))
+            {
+                return "a scalene triangle";
+            }
+            else if ((_side1 == _side2) && (_side1 == _side3))
+            {
+                return "an equilateral triangle";
+            }
+            else
+            {
+                return "an isosceles triangle";
+            }
+        }
     }
-  public void SetSide3(int value)
-  {
-    _side3 = value;
-  }
-  public string CheckType()
-  {
-    if ((_side1 > (_side2 + _side3)) || (_side2 > (_side1 + _side3)) || (_side3 > (_side1 + _side2)))
-    {
-      return "not a triangle";
-    }
-    else if ((_side1 != _side2) && (_side1 != _side3) && (_side2 != _side3))
-    {
-      return "a scalene triangle";
-    }
-    else if ((_side1 == _side2) && (_side1 == _side3))
-    {
-      return "an equilateral triangle";
-    }
-    else
-    {
-      return "an isosceles triangle";
-    }
-  }
 }
--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -2,16 +2,71 @@
 
 namespace ShapeTracker
 {
-class Program
-{
-  static void Main()
-  {
-    Triangle testTriangle = new Triangle(50, 50, 50);
-    Console.WriteLine($"Side one of the triangle: {testTriangle.GetSide(1)}");
-    Console.WriteLine($"Side two of the triangle: {testTriangle.GetSide(2)}");
-    Console.WriteLine($"Side three of the triangle: {testTriangle.GetSide(3)}");
-    Console.WriteLine($"This is {testTriangle.CheckType()}.");
-  }
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("//--//--//--//--//--//--//--//");
+            Console.WriteLine("//--I AM THE SHAPE TRACKER--//");
+            Console.WriteLine("Please enter three sides for me to evaluate:");
+            Console.WriteLine("Enter the first side length:");
+            string stringNumber1 = Console.ReadLine();
+            Console.WriteLine("Enter the second side:");
+            string stringNumber2 = Console.ReadLine();
+            Console.WriteLine("Enter the final side:");
+            string stringNumber3 = Console.ReadLine();
+            int length1 = int.Parse(stringNumber1);
+            int length2 = int.Parse(stringNumber2);
+            int length3 = int.Parse(stringNumber3);
+            Triangle tri = new Triangle(length1, length2, length3);
+            ConfirmOrEditTriangle(tri);
+        }
 
-}
+        static void ConfirmOrEditTriangle(Triangle tri)
+        {
+            Console.WriteLine("Your chosen triangle is of lengths as follows...");
+            Console.WriteLine($"Side 1 is of length {tri.Side1}");
+            Console.WriteLine($"Side 2 is of length {tri.Side2}");
+            Console.WriteLine($"Side 3 is of length {tri.Side3}");
+            Console.WriteLine("If all is correct enter 'y' otherwise, enter 'n' to start again");
+            string userInput = Console.ReadLine();
+            if (userInput == "y")
+            {
+                CheckTriangleType(tri);
+            }
+            else
+            {
+                Console.WriteLine("Please re-enter three sides for me to evaluate:");
+                Console.WriteLine("Enter the first side length:");
+                string stringNumber1 = Console.ReadLine();
+                Console.WriteLine("Enter the second side:");
+                string stringNumber2 = Console.ReadLine();
+                Console.WriteLine("Enter the final side:");
+                string stringNumber3 = Console.ReadLine();
+                tri.Side1 = int.Parse(stringNumber1);
+                tri.Side2 = int.Parse(stringNumber2);
+                tri.Side3 = int.Parse(stringNumber3);
+                ConfirmOrEditTriangle(tri);
+            }
+        }
+
+        static void CheckTriangleType(Triangle tri)
+        {
+            string result = tri.CheckType();
+            Console.WriteLine("//--//--//--//--//--//--//--//");
+            Console.WriteLine($"This is {result}.");
+            Console.WriteLine("//--//--//--//--//--//--//--//");
+            Console.WriteLine("Care to play again?");
+            Console.WriteLine("Enter 'n' to begin another experiment with triangles");
+            string userInput = Console.ReadLine();
+            if (userInput == "n" || userInput == "N")
+            {
+                Main();
+            }
+            else
+            {
+                Console.WriteLine("Adieu!");
+            }
+        }
+    }
 }
