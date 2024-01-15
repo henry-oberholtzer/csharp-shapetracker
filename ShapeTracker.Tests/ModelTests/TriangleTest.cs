dc931d0f8ed425c5f20b32fdcb06567658078a78
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-01-15 15:30:01 -0800

    passing test 2

--- ShapeTracker.Tests/ModelTests/TriangleTest.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTest.cs
@@ -8,8 +8,17 @@ namespace ShapeTracker.Tests
         [TestMethod]
         public void TriangleConstructor_CreateInstanceOfTriangle_Triangle()
         {
-            Triangle newTriangle = new Triangle();
+            Triangle newTriangle = new Triangle(3);
             Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+        }
+
+        [TestMethod]
+        public void GetSide1_ReturnsSide1_Int()
+        {
+            int length1 = 3;
+            Triangle newTriangle = new Triangle(length1);
+            int result = newTriangle.Side1;
+            Assert.AreEqual(length1, result);
         }
     }
 }
