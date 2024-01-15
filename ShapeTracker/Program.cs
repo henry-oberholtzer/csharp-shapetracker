24b6ac4f467a51c742507e7bcf21c984ef4e6a64
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-01-14 17:08:59 -0800

    done for saturday, left off at apie

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -7,10 +7,10 @@ class Program
   static void Main()
   {
     Triangle testTriangle = new Triangle(50, 50, 50);
-    Console.WriteLine($"Side one of the triangle: {testTriangle.Side1}");
-    Console.WriteLine($"Side two of the triangle: {testTriangle.Side2}");
-    Console.WriteLine($"Side three of the triangle: {testTriangle.Side3}");
-    Console.WriteLine($"This is {testTriangle.CheckType()}.")
+    Console.WriteLine($"Side one of the triangle: {testTriangle.GetSide(1)}");
+    Console.WriteLine($"Side two of the triangle: {testTriangle.GetSide(2)}");
+    Console.WriteLine($"Side three of the triangle: {testTriangle.GetSide(3)}");
+    Console.WriteLine($"This is {testTriangle.CheckType()}.");
   }
 
 }
